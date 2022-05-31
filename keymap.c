843e139e12fd3397d47ed79c5be1bf2cbd99f9f0
grungeee <sharapanikita@gmail.com>
2022-06-01 01:40:30 +0200

    Did it a few months ago, gotta make a more refined vesion in the future

--- keymap.c
+++ keymap.c
@@ -1,13 +1,16 @@
 /* to compile this code put the next command in QMK MSYS */
 /* qmk compile -kb themadnoodle/noodlepad -km grungecorpse */
-
-
 #include QMK_KEYBOARD_H
 
-
 enum custom_keycodes {
-    LAYER_INCREMENT = SAFE_RANGE,
-    PASTE_A_LINK,
+    PASTE_A_LINK = SAFE_RANGE,
+    LAYER_0,
+    LAYER_1,
+    LAYER_2,
+    LAYER_3,
+    LAYER_4,
+    SCREENSHOT,
+    COLOR_PICKER,
     M01,
     M02,
     M03,
@@ -53,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [0] = LAYOUT_ortho_3x3(
-      KC_MPRV, KC_MUTE, KC_MNXT,
-      KC_MSTP, KC_MPLY, KC_MSEL,
-      LAYER_INCREMENT, KC_MAIL, KC_MYCM
+      LT(15, KC_MPRV), M08, M09,
+      M04, M05, M06,
+      M01, M02, M03
       ),
 
 
@@ -70,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [1] = LAYOUT_ortho_3x3(
-      KC_TRNS, KC_TRNS, KC_TRNS,
-      LWIN(KC_1), LWIN(KC_2), LWIN(KC_3),
-      LAYER_INCREMENT, LALT(KC_TAB), PASTE_A_LINK
+      LT(15, M17), M18, M19,
+      M14, M15, M16,
+      M11, M12, M13
       ),
 
 
@@ -86,16 +89,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------'
      */
 
-    [2] = LAYOUT_ortho_3x3(
+    [2] = LAYOUT_ortho_3x3( // LAYER 3
 
-      M27, M28, M29,
+      LT(15, M27), M28, M29,
       M24, M25, M26,
-      LAYER_INCREMENT, M22, M23
+      M21, M22, M23
+      ),
+
+
+    
+
+    [3] = LAYOUT_ortho_3x3( // LAYER 4
+
+      LT(15,KC_NO), KC_NO, KC_NO,
+      SCREENSHOT, COLOR_PICKER, KC_NO,
+      KC_NO, KC_NO, KC_NO
+      ),
+
+    [4] = LAYOUT_ortho_3x3( // LAYER 4
+
+      LT(15,KC_NO), KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO
+      ),
+
+    [15] = LAYOUT_ortho_3x3( // LAYER WITH LAYERS
+
+      KC_NO, KC_NO, KC_NO,
+      LAYER_3, LAYER_4, KC_NO,
+      LAYER_0, LAYER_1, LAYER_2
       )
 
 
 
-
+/*
+      KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO
+      )
+      */
       /*
       KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS,
@@ -108,70 +140,328 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       M21, M22, M23
       )
       */
+
 };
 
+    /* LAYER 0
+     * ,-----------------------.
+     * |   <<  |  MUTE |  >>   |  ENCODER - PRESS (MUTE) / KNOB (VOLUME CONTROL)
+     * |-------+-------+-------|
+     * |  STOP |  PLAY | MEDIA |
+     * |-------+-------+-------|
+     * | CALC  | MAIL  | PC/FN |
+     * `-----------------------'
+     */
 
 
-
-//Switch between Leyers
-    
-
-int LAYER_LEVEL = 0;
- 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     switch (keycode) {
-      case LAYER_INCREMENT:
-         if (record->event.pressed) {
-             ++LAYER_LEVEL;
-             if (LAYER_LEVEL > 2) {
-                 LAYER_LEVEL = 0;
-          } else {
-
-          }
-              layer_move(LAYER_LEVEL);
-        } 
-        break;
-        return true;
-/**/
-    
-
-    case PASTE_A_LINK:
-      if (record->event.pressed) {
-            //keydown event
-            register_code(KC_LCTRL);
-            register_code(KC_LSFT);
-            register_code(KC_LEFT);
-            unregister_code(KC_LCTRL);
-            unregister_code(KC_LSFT);
-            unregister_code(KC_LEFT);
-            register_code(KC_LCTRL);
-            register_code(KC_K);
-            unregister_code(KC_LCTRL);
-            unregister_code(KC_K);
-            register_code(KC_LCTRL);
-            register_code(KC_V);
-            unregister_code(KC_LCTRL);
-            unregister_code(KC_V);
+/// Layer switching ///
+      case LAYER_0:
+          if (record->event.pressed) {
             
-            } else {
-            //keyup event
-            tap_code_delay(KC_ENTER, 80);
-            } 
-      break;
+          } else {
+            layer_move(0);
           }
-          return true;
-        
+          break;
+
+      case LAYER_1:
+          if (record->event.pressed) {
+            
+          } else {
+            layer_move(1);
+          }
+          break;
+
+      case LAYER_2:
+          if (record->event.pressed) {
+            
+          } else {
+            layer_move(2);
+          }
+          break;
+
+      case LAYER_3:
+          if (record->event.pressed) {
+            
+          } else {
+            layer_move(3);
+          }
+          break;
+
+      case LAYER_4:
+          if (record->event.pressed) {
+            
+          } else {
+            layer_move(4);
+          }
+          break;
+
+/// Layer 0 Custom Keys ///
+    
+      case M01:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_F13);
+          }
+         break;
+
+      case M02:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN(SS_LSFT("s")));
+          }
+         break;
+
+      case M03:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MYCM);
+          }
+         break;
+
+      case M04:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MSTP);
+          }
+         break;
+
+      case M05:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MPLY);
+          }
+         break;
+
+      case M06:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MSEL);
+          }
+         break;
+
+      case M07:
+         if (record->event.pressed) {
+            register_code(KC_MPRV);
+          } else {
+            unregister_code(KC_MPRV);
+          }
+         break;
+
+      case M08:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MUTE);
+          }
+         break;
+
+      case M09:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_MNXT);
+          }
+         break;
+
+/// Layer 1 Custom Keys ///
+      case M11:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M12:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M13:
+         /// Paste a link macro ///
+         if (record->event.pressed) {
+           SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))));
+           SEND_STRING(SS_LCTL("k"));
+           SEND_STRING(SS_DELAY(200) SS_LCTL("v") SS_DELAY(200));
+           SEND_STRING(SS_TAP(X_ENTER));
+          } 
+          break;
+    
+      case M14: // Key 1
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN("1"));
+          }
+         break; 
+
+      case M15:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN("2"));
+          }
+         break;
+
+      case M16:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN("3"));
+          }
+         break;
+
+      case M17:
+         if (record->event.pressed) {
+            SEND_STRING(SS_LWIN(SS_TAP(X_TAB)));
+          } else {
+            
+          }
+         break;
+
+      case M18:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M19:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN("d"));
+          }
+         break;
+
+/// Layer 2 Custom Keys ///
+
+      case M21: // Key 1
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+            //rgblight_mode(10);
+          }
+         break; 
+
+      case M22:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M23:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M24:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LCTL(SS_LWIN(SS_LALT("1"))));
+          }
+         break;
+
+      case M25:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LCTL(SS_LWIN(SS_LALT("2"))));
+          }
+         break;
+
+      case M26:
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LCTL(SS_LWIN(SS_LALT("3"))));
+          }
+         break;
+
+      case M27:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M28:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+      case M29:
+         if (record->event.pressed) {
+            
+          } else {
+            tap_code(KC_NO);
+          }
+         break;
+
+/// Layer 3 Custom Keys ///
+
+      case SCREENSHOT: // Key 4
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN(SS_LSFT("S")));
+          }
+         break;
+
+      case COLOR_PICKER: // Key 5
+         if (record->event.pressed) {
+            
+          } else {
+            SEND_STRING(SS_LWIN(SS_LSFT("C")));
+          }
+         break;
+
+
+        } 
+        return true;
 };
 
 
-/* Colors for layers*/
+
+
+///Sets the default color///
+  void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom();
+  rgblight_sethsv_noeeprom(85, 255, 255);
+}
+
+/// Colors for layers///
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 0:
         rgblight_setrgb (0x00,  0xFF, 0x00);
         break;
+
     case 1:
         rgblight_setrgb (0xFF,  0x00, 0xFF);
         break;
@@ -179,30 +469,41 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case 2: //  for any other layers, or the default layer
         rgblight_setrgb (0x00,  0x00, 0xFF);
         break;
-    }
+    
+    case 3: //  for any other layers, or the default layer
+        rgblight_setrgb (201 , 60 , 0);
+        break;
+
+    case 4: //  for any other layers, or the default layer
+        rgblight_setrgb (255 , 61 , 168);
+        break;
+        }
+
   return state;
 }
 
   
-/*Sets the default color*/
-  void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom();
-  rgblight_sethsv_noeeprom(85, 255, 255);
-}
-
+///ENCODER///
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
   switch (get_highest_layer(layer_state)) {
       case 1:
-           if (clockwise) {
-              tap_code(KC_RGHT);
+          if (clockwise) {
+              SEND_STRING(SS_LCTL(SS_TAP(X_TAB)));
           } else {
-              tap_code(KC_LEFT);
+              SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_TAB))));
           }
           break;
       case 2:
           if (clockwise) {
-              tap_code(KC_RGHT);
+              SEND_STRING(SS_LCTL(SS_LWIN(SS_TAP(X_RIGHT))));
+          } else {
+              SEND_STRING(SS_LCTL(SS_LWIN(SS_TAP(X_LEFT))));
+          }
+          break;
+      case 3:
+          if (clockwise) {
+              tap_code(KC_RIGHT);
           } else {
               tap_code(KC_LEFT);
           }
@@ -218,138 +519,3 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
   }
     return true;
 }
-
-/*
-enum combo_events {
-  LAYER_0,
-  LAYER_1
-};
-
-
-const uint16_t PROGMEM LAYER_0_COMBO[] = {M25, M22, COMBO_END};
-const uint16_t PROGMEM LAYER_1_COMBO[] = {M26, M23, COMBO_END};
-
-combo_t key_combos[] = {
-  [LAYER_0] = COMBO_ACTION(LAYER_0_COMBO),
-  [LAYER_1] = COMBO_ACTION(LAYER_1_COMBO),
-};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case LAYER_0:
-      if (pressed) {
-        SEND_STRING("GUCCI");
-      }
-      break;
-
-      case LAYER_1:
-      if (pressed) {
-        SEND_STRING("COOCHIE");
-      }
-      break;
-  }
-}
-*/
-/*
-enum combos { 
-  M08_M02_,
-  M08_M03_,
-  
-};
-
-const uint16_t PROGMEM m08_m02_[] = { M25, M22, COMBO_END};
-const uint16_t PROGMEM m08_m03_[] = { M26, M23, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  [M08_M02_] = COMBO(m08_m02_,COMBO_END),
-  [M08_M03_] = COMBO(m08_m03_,COMBO_END),
-  
-
-};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case M25_M22_:
-      if (pressed) {
-        layer_move(0);
-      }
-      break;
-
-      case M26_M23_:
-      if (pressed) {
-        layer_move(1);
-      }
-      break;
-  }
-}
-
-*/
-
-enum combos { 
-  M08_M02_,
-  M08_M03_,
-  M18_M11_,
-  M18_M13_,
-  M28_M21_,
-  M28_M22_,
-  
-};
-
-const uint16_t PROGMEM m08_m02_[] = { M08, M02, COMBO_END};
-const uint16_t PROGMEM m08_m03_[] = { M08, M03, COMBO_END};
-const uint16_t PROGMEM m18_m11_[] = { M18, M11, COMBO_END};
-const uint16_t PROGMEM m18_m13_[] = { M18, M13, COMBO_END};
-const uint16_t PROGMEM m28_m21_[] = { M28, M21, COMBO_END};
-const uint16_t PROGMEM m28_m22_[] = { M28, M22, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  [M08_M02_] = COMBO(m08_m02_,COMBO_END),
-  [M08_M03_] = COMBO(m08_m03_,COMBO_END),
-  [M18_M11_] = COMBO(m18_m11_,COMBO_END),
-  [M18_M13_] = COMBO(m18_m13_,COMBO_END),
-  [M28_M21_] = COMBO(m28_m21_,COMBO_END),
-  [M28_M22_] = COMBO(m28_m22_,COMBO_END),
-  
-};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-
-    case M08_M02_:
-      if (pressed) {
-        layer_move(1);
-      }
-      break;
-
-      case M08_M03_:
-      if (pressed) {
-        layer_move(2);
-      }
-      break;
-
-      case M18_M11_:
-      if (pressed) {
-        layer_move(0);
-      }
-      break;
-
-      case M18_M13_:
-      if (pressed) {
-        layer_move(2);
-      }
-      break;
-
-       case M28_M21_:
-      if (pressed) {
-        layer_move(0);
-      }
-      break;
-
-      case M28_M22_:
-      if (pressed) {
-        layer_move(1);
-      }
-      break;
-  }
-}
-
